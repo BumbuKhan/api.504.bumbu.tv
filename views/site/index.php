cf76486e69e89810d02eb5cf55fa41ff3eb7187e
???????±?°?? ???????±?°?????? <qurban.qranov93@gmail.com>
2017-05-13 00:11:06 +0400

    Hardcoded domain address updated to dynamic one

--- views/site/index.php
+++ views/site/index.php
@@ -1,7 +1,12 @@
+<?php
+use yii\helpers\Url;
+
+?>
+
 <h1>Documentation</h1>
 <hr>
-<code>[http://api.504.bumbu.tv/web/lessons]</code> - will return all available lessons divided by pagination <br>
-<code>[http://api.504.bumbu.tv/web/lessons/1]</code> - will return only the first lesson's data (!WITHOUT WORDS) <br>
-<code>[http://api.504.bumbu.tv/web/lessons/1?expand=words]</code> - will return all words which contain in lesson 1 along with it's data <br>
-<code>[http://api.504.bumbu.tv/web/words]</code> - will return all words available divided by pagination <br>
-<code>[http://api.504.bumbu.tv/web/words/1]</code> - will return only the first word's data <br>
+<code>[<?=Url::base(true)?>/lessons]</code> - will return all available lessons divided by pagination <br>
+<code>[<?=Url::base(true)?>/lessons/1]</code> - will return only the first lesson's data (!WITHOUT WORDS) <br>
+<code>[<?=Url::base(true)?>/lessons/1?expand=words]</code> - will return all words which contain in lesson 1 along with it's data <br>
+<code>[<?=Url::base(true)?>/words]</code> - will return all words available divided by pagination <br>
+<code>[<?=Url::base(true)?>/words/1]</code> - will return only the first word's data <br>
