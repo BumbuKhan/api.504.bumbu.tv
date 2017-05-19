ed3780f8ac5be7072cb459162c2862e65d7630be
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-05-19 21:08:22 +0400

    Added CORS filters to 'Words' model, also removed pagination

--- controllers/WordController.php
+++ controllers/WordController.php
@@ -2,6 +2,8 @@
 namespace app\controllers;
 
 use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
+use app\models\Words;
 
 class WordController extends ActiveController
 {
@@ -10,4 +12,32 @@ class WordController extends ActiveController
         'class' => 'yii\rest\Serializer',
         'collectionEnvelope' => 'items',
     ];
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        // add CORS filter
+        $behaviors['corsFilter'] = [
+            'class' => \yii\filters\Cors::className(),
+        ];
+
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions = parent::actions();
+        unset($actions['index']);
+        return $actions;
+    }
+
+    public function actionIndex()
+    {
+        $activeData = new ActiveDataProvider([
+            'query' => Words::find(),
+            'pagination' => false
+        ]);
+        return $activeData;
+    }
 }
