01197afd487eea7b39d509d3f1148695988b2e18
BumbuKhan <qurban.qurbanov93@gmail.com>
2017-05-30 22:44:05 +0400

    Resources updated

--- views/content/help.php
+++ views/content/help.php
@@ -5,12 +5,26 @@ use yii\helpers\Url;
 
 <p>Actually there are only two things you should bare in mind while filling up <b>'Example a'</b>, <b>'Example b'</b> and <b>'Example c'</b> fields</p>
 <br>
-<p><b>1. Wrap word in a single asterisk</b> (*like this*) if that word is used for current word, that will make it <b>bold</b>
-    <img src="<?=Url::base(true)?>/img/img_1.jpg" alt="" class="img-responsive">
+<p><b>1. Wrap word in a single asterisks</b> (*like this*) if that word is used for current word, that will make it <b>bold</b>
+    <img src="<?=Url::base(true)?>/img/img_1.jpg" alt="" class="img-responsive" style="border: 1px #cccccc solid; padding: 10px; margin-top: 5px;">
 </p>
 
 <br>
-<p><b>2. Wrap word in a double asterisk</b> (**like this**) if that word is from one of th previous lesson (or perhaps from current), that will make it <span style="text-decoration: underline"><b>bold and underlined</b></span>
-    <img src="<?=Url::base(true)?>/img/img_2.jpg" alt="" class="img-responsive">
+<p><b>2. Wrap word in a double asterisks</b> (**like this**) if that word is from one of th previous lesson (or perhaps from current), that will make it <span style="text-decoration: underline"><b>bold and underlined</b></span>
+    <img src="<?=Url::base(true)?>/img/img_2.jpg" alt="" class="img-responsive" style="border: 1px #cccccc solid; padding: 10px; margin-top: 5px;">
+</p>
 
+<h3>Material to fill up</h3>
+<p>Unfortunately there isn't any document to copy-paste, so you can find below a list of shots, divided by lessons</p>
+
+<p>
+    <a href="<?=Url::base(true)?>/img/Lesson_1_part_1.JPG" target="_blank">Lesson 1 (part 1)</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?=Url::base(true)?>/img/Lesson_1_part_2.JPG" target="_blank">Lesson 1 (part 2)</a>
+    <br>
+    <a href="<?=Url::base(true)?>/img/Lesson_2_part_1.JPG" target="_blank">Lesson 2 (part 1)</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?=Url::base(true)?>/img/Lesson_3_part_2.JPG" target="_blank">Lesson 2 (part 2)</a>
+    <br>
+    <a href="<?=Url::base(true)?>/img/Lesson_3_part_1.JPG" target="_blank">Lesson 3 (part 1)</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?=Url::base(true)?>/img/Lesson_3_part_2.JPG" target="_blank">Lesson 3 (part 2)</a>
+    <br>
+    <a href="<?=Url::base(true)?>/img/Lesson_4_part_1.JPG" target="_blank">Lesson 4 (part 1)</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?=Url::base(true)?>/img/Lesson_4_part_2.JPG" target="_blank">Lesson 4 (part 2)</a>
+    <br>
+    <a href="<?=Url::base(true)?>/img/Lesson_5_part_1.JPG" target="_blank">Lesson 5 (part 1)</a>&nbsp;&nbsp;|&nbsp;&nbsp;<a href="<?=Url::base(true)?>/img/Lesson_5_part_2.JPG" target="_blank">Lesson 5 (part 2)</a>
 </p>
